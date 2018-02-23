37bab95f43a2f4ef3a5910a2b7937b422261b0ea
coolmichaelfritz <fritz.michael11@gmail.com>
2018-02-23 13:47:24 -0800

    add vars for all entry fields in the scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   $("form#restaurant").submit(function() {
+    var userName = $("input#name").val();
+    var userRadio1 = $("input:radio[name=inlineRadioOptions]:checked").val();
+    var userTemp = parseInt($("input#temperature").val());
+    var userRadio2 = $("input:radio[name=inlineRadioOptions2]:checked").val();
+    var userAge = parseInt($("input#age").val());
+
+
     var cityLocation = $("input:radio[name=cityLocation]:checked").val();
     var result;
     if (cityLocation === "eastSide") {
